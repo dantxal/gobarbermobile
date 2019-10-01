b482de5bcc59000ccc8abaef41d160fb018d35cd
dantxal <dantxal@gmail.com>
2019-10-01 10:26:28 -0300

    Basic ESlint, Prettier, Edconf and RootImport

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+import Teste from '~/Teste';
+
+export default function App() {
+  return (
+    <View>
+      <Teste />
+    </View>
+  );
+}
